08396d68e19d9cd8e21a7b58aae6afe31b7cd212
Ayshi-Moni-0803 <ayshimoni0803@gmail.com>
2023-04-04 18:42:48 +0600

    module one

--- hi.c
+++ hi.c
@@ -1,6 +1,52 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hi I am a student but I am very lazy and halka adding");
+    int a=1000000000;
+    long long int b=100000000000;
+    printf("%lld",b);
     return 0;
 }
+
+    // printf("hlw world\n");
+    // printf("I am Ayshi Moni\n");
+    // printf("notun line e jaw\n");
+    // printf("I am /  \t \t \t      strong girl\n");
+    // printf("//\n");
+    // printf("%%\n");
+
+
+    // int rahim=10;
+    // float karim=10.52;
+    // char habib='XYZ';
+    // printf("%d %0.2f %c",rahim,karim,habib);
+
+
+
+
+    // int rahim,karim;
+    // char c;
+    // float f;
+    // scanf("%d %c %f",&rahim,&c,&f);
+    // printf("%d %c,%0.2f", rahim,c,f);
+
+
+
+
+//     #include<stdio.h>
+// int main()
+// {   int a,b;
+//     char p;
+//     scanf("%d%c %d%c", &a,&p,&b,&p);
+//     printf("%d%% %d%%",a,b);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {   int a,b;
+    
+//     scanf("%d%% %d%%", &a,&b);
+//     printf("%d%% %d%%",a,b);
+//     return 0;
+// }
